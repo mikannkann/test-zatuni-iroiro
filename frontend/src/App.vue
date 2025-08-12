33db79791f26ed76c3e52d9672a2a3f57b9fd9ba
mikannkann <82451989+mikannkann@users.noreply.github.com>
2025-08-13 02:44:37 +0900

    feat: 意図した依存関係エラー

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import fake from 'not-a-real-package'
 </script>
 
 <template>
@@ -8,7 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg=123 />
+      <HelloWorld msg="You did it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
