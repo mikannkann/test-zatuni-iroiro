28879c306bf8fc824e8b68c2233c827278fe373c
mikannkann <82451989+mikannkann@users.noreply.github.com>
2025-08-13 02:30:45 +0900

    feat: 意図した構文エラーを含んだコミット

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+const a = ; // 構文エラー
 </script>
 
 <template>
@@ -17,7 +18,8 @@ import HelloWorld from './components/HelloWorld.vue'
     </div>
   </header>
 
-  <RouterView />
+  <RouterView >
+  <div>未閉じタグ
 </template>
 
 <style scoped>
