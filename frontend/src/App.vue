4231e8011bf56f09b7443eedc0fa391b8fd9550d
mikannkann <82451989+mikannkann@users.noreply.github.com>
2025-08-13 02:45:40 +0900

    Revert "feat: 意図した依存関係エラー"
    This reverts commit 33db79791f26ed76c3e52d9672a2a3f57b9fd9ba.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-import fake from 'not-a-real-package'
 </script>
 
 <template>
@@ -9,7 +8,7 @@ import fake from 'not-a-real-package'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg=123 />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
