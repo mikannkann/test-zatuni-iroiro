3213f8ba5b1fb84ef49bf6b9789cf92676766667
mikannkann <82451989+mikannkann@users.noreply.github.com>
2025-08-13 11:42:15 +0900

    fix: タグ名のミスを含んだ改善

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
 
-        <HelloWorld msg="You did it!" /
+        <HeloWorld msg="You did it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
